c3bc7aa2a1c2b20b4396a1eee33974082a209019
Nami-Doc <vendethiel@hotmail.fr>
2014-03-31 10:10:57 +0200

    add graphs

--- plugins/cans/pages/history.php
+++ plugins/cans/pages/history.php
@@ -13,11 +13,15 @@ if (!defined('IN_ICYPHOENIX'))
 	die('Hacking attempt');
 }
 
+// This page was made with extensibility in mind
+// Adding the ability to view history for several products
+//  should be very easy.
 $id = request_var('id', 0);
 $sql = 'SELECT *
 	FROM ' . CANS_TABLE . '
 	WHERE id = ' . $id;
 $result = $db->sql_query($sql);
+$series = array();
 if (!($can = $db->sql_fetchrow($result)))
 	message_die(GENERAL_ERROR, "Can't find can");
 $db->sql_freeresult($result);
@@ -33,11 +37,40 @@ $sql = 'SELECT ch.*, u.username, u.user_color, u.user_active
 		LEFT JOIN ' . USERS_TABLE . ' u ON u.user_id = ch.user_id
 	WHERE ch.can_id = ' . $id;
 $result = $db->sql_query($sql);
+$history_data = array();
 while ($row = $db->sql_fetchrow($result))
 {
+	// prepare graph data
+	$d = '' . ($row['date'] * 1000);
+	if (empty($history_data[$d]))
+		$history_data[$d] = 1;
+	else
+		$history_data[$d]++;
+	
+	// assign templates vars
 	$template->assign_block_vars('cans.history', array(
 		'U_USER' => $row['user_id'] ? colorize_username($row['user_id'], $row['username'], $row['user_color'], $row['user_active']) : '',
-		'DATE' => date('Y/m/d', $row['date']),
+		'DATE' => date('d/m/Y', $row['date']),
 	));
 }
 $db->sql_freeresult($result);
+ksort($history_data); // really, that should never be needed
+$series[] = array(
+	'name' => $can['name'],
+	'data' => associative_array_to_tuples($history_data),
+);
+
+$template->assign_var('SERIES', json_encode($series));
+
+$template->js_include[] = '../../plugins/cans/templates/common/js/highcharts.js';
+
+// transforms an associative array to a list of tuples
+// can also apply a transform fn
+function associative_array_to_tuples($array, $f = null)
+{
+	$ret = array();
+	foreach ($array as $k => $v)
+		// convert to float for json_encode (int overflows)
+		$ret[] = array((float) $k, $v);
+	return $ret;
+}
