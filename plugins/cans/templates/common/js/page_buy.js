ee21ba57483e9ca41466cc080b6f8b46dc1c819f
tagtaco <battello.theo@gmail.com>
2014-04-04 17:23:35 +0200

    Improvements to page_buy.js

--- plugins/cans/templates/common/js/page_buy.js
+++ plugins/cans/templates/common/js/page_buy.js
@@ -1,21 +1,25 @@
 $(function () {
-	var vals = {};
+	var banks = {};
+	var total = can.price;
 
 	function fetchMoney() {
 		var _self = this,
 			val = this.value;
-		if (vals.hasOwnProperty(val))
-		{
-			$user_money.html(vals[val]);
+		if (banks.hasOwnProperty(val)) {
+			$user_money.html(banks[val]);
+			updateBankBalance();
 			return;
 		}
-		if (!val)
-			return;
+		if (!val) // uncheck "use acc"
+			return disableUseAccInput();
 
 		$user_money.html('...');
 		$.get('cans.php', {page: 'ajax', mode: 'money', username: val }, function (data) {
-			vals[val] = data;
-			$user_money.html(vals[_self.value] || ''); // in case it changed
+			if (!data) // no data, no user
+				return disableUseAccInput();
+
+			banks[val] = data;
+			updateBankBalance();
 		});
 	}
 
@@ -26,12 +30,20 @@ $(function () {
 			fetchMoney.call(event.target);
 		});
 
+	var $use_acc = $('<div><input type="checkbox" name="use_acc">Utiliser le compte</div>');
+	var $use_acc_input = $use_acc.find('input').change(function () {
+		if (!banks[$user_id.val()]) // can't buy
+			return disableUseAccInput();
+		updateBankBalance();
+	});
+	function disableUseAccInput() {
+		$use_acc_input.removeAttr('checked');
+	}
+
 	var $user_money = $('<span>')
 		.insertAfter($user_id);
 	$('<span>&euro;</span>').insertAfter($user_money)
-	.after(
-		$('<div><input type="checkbox" name="use_acc">Utiliser le compte</div>')
-	);
+	.after($use_acc);
 
 	var $input_number = $('[name=count]');
 	var $total_price = $('<span>').html('Prix total : ' + can.price + ' &euro;')
@@ -40,7 +52,23 @@ $(function () {
     	var val = $input_number.val();
     	if (/[^0-9]/.test(val))
     		$input_number.val(val = val.replace(/[^0-9]/g, ''));
-		var total = Math.round((val * can.price) * 100) / 100;
+		total = Math.round((val * can.price) * 100) / 100;
     	$total_price.html('Prix total : ' + total + ' &euro;');
+    	updateBankBalance();
 	});
+
+	function updateBankBalance() {
+		var money = banks[$user_id.val()];
+		if (!money || !$use_acc_input.is(':checked'))
+			return $user_money.html(money ? money : "");
+	
+		var rest = Math.round((money - total) * 100) / 100;
+		$user_money.html(money + " - " + total + " = \
+			<span " + color_for(rest) + "> " + rest + " </span>");
+		$("input[name=save]").prop("disabled", rest < 0);	
+	}
 });
+
+function color_for(rest) {
+	return "style='color: " + (rest < 0 ? 'red' : 'green') + "'";
+}
